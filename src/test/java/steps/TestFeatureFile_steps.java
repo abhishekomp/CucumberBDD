70d16cd9d37b80fb3efcff9f700466a9812fbfc1
abhishek <abhishek2283@hotmail.com>
2021-10-11 22:02:25 +0200

    reading json array

--- src/test/java/steps/TestFeatureFile_steps.java
+++ src/test/java/steps/TestFeatureFile_steps.java
@@ -20,7 +20,7 @@ public class TestFeatureFile_steps {
 	    System.out.println("GITHUB_RUN_URL = " + System.getenv("GITHUB_RUN_URL"));
 
 //		System.out.println("icc are = ");
-//		JSONObject payload = new JSONObject(clientPyaload);
+		JSONObject payload = new JSONObject(clientPyaload);
 //		JSONArray icc = payload.getJSONArray("icc");
 //		icc.forEach(System.out::println);
 
