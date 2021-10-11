24f677239cc2276d95b6b21be53468cb078ef29a
abhishek <abhishek2283@hotmail.com>
2021-10-11 16:19:36 +0200

    multiple scenarios

--- src/test/java/steps/TestFeatureFile_steps.java
+++ src/test/java/steps/TestFeatureFile_steps.java
@@ -12,6 +12,8 @@ public class TestFeatureFile_steps {
 	    System.out.println("Inside I have numbers");
 	    System.out.println("Job id = " + System.getenv("JOB_ID_TRS"));
 	    System.out.println("Example Key from Postman = " + System.getenv("TEST_ORDER_NUM"));
+	    String clientPyaload = System.getenv("TEST_CLIENT_PAYLOAD");
+	    System.out.println("clientPyaload = " + clientPyaload);
 	    System.out.println("GITHUB_RUN_URL = " + System.getenv("GITHUB_RUN_URL"));
 	}
 
