e019d22c21d434dcaea05f4dafe15def2b2eada6
abhishek2283java <75280148+abhishek2283java@users.noreply.github.com>
2021-10-12 14:55:34 +0200

    Update TestFeatureFile_steps.java

--- src/test/java/steps/TestFeatureFile_steps.java
+++ src/test/java/steps/TestFeatureFile_steps.java
@@ -13,6 +13,7 @@ public class TestFeatureFile_steps {
 	public void i_have_numbers() {
 	    System.out.println("Inside I have numbers");
 	    System.out.println("Job id = " + System.getenv("JOB_ID_TRS"));
+            System.out.println("Test OrderNumber = " + System.getenv("TEST_ORDERNUM"));
 	    System.out.println("Example Key from Postman = " + System.getenv("TEST_ORDER_NUM"));
 	    String clientPyaload = System.getenv("TEST_CLIENT_PAYLOAD");
 	    System.out.println("clientPyaload = " + clientPyaload);
